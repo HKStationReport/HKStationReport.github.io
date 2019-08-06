f72a3f878058a58a87050b9bafcb74977a46abf1
HKStationReport <53723569+HKStationReport@users.noreply.github.com>
2019-08-06 16:04:00 +0800

    Update post.js

--- post.js
+++ post.js
@@ -1,7 +1,6 @@
-//var url = 'http://demo.thingsboard.io:80/api/v1/VdmXEJGmafDt4ag8IcTJ/telemetry';
-var url = 'https://postman-echo.com/post';
-var data = {username: 'example'};
-//var data = {'Station':'Tin Shui Wai','Latitude':22.4477,'Longitude':114.00400000000002,'Status':'Police','Comment':'TSW hi comment here'};
+var url = 'http://demo.thingsboard.io:80/api/v1/VdmXEJGmafDt4ag8IcTJ/telemetry';
+//var data = {"username": "example"};
+var data = {"Station":"Tin Shui Wai","Latitude":22.4477,"Longitude":114.00400000000002,"Status":"Police","Comment":"TSW hi comment here"};
 var myHeaders = new Headers();
 myHeaders.append('Content-Type', 'application/json'); // This one sends body
 
@@ -9,15 +8,17 @@ myHeaders.append('Content-Type', 'application/json'); // This one sends body
 console.log(data);
 console.log(JSON.stringify(data));
 
-//body:  JSON.stringify(data),
+
 //headers: myHeaders,
+//body: "{\"Username\":\"example\"}",
+//{  "content-type": "application/json"},
+
+
 fetch(url, {
   method: "POST",
-  body: "{\"username\":\"example\"}",
-  headers: {
-  "content-type": "application/json"
-},
-  mode: "cors"
+  body:  JSON.stringify(data),  
+  headers: myHeaders,
+  mode: "no-cors"
 }).then(res => res.json())
 .catch(error => console.error('Error:', error))
 .then(response => console.log('Success:', response));
