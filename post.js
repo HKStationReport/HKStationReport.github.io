7d0cd6b2aea3a693fc51fa8f9fa2877a42868ce1
HKStationReport <53723569+HKStationReport@users.noreply.github.com>
2019-08-06 16:10:39 +0800

    Update post.js

--- post.js
+++ post.js
@@ -18,7 +18,7 @@ fetch(url, {
   method: "POST",
   body:  JSON.stringify(data),  
   headers: myHeaders,
-  mode: "cors"
+  mode: "same-origin"
 }).then(res => res.json())
 .catch(error => console.error('Error:', error))
 .then(response => console.log('Success:', response));
