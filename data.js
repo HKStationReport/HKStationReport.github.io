291ac867e2a629ea3001f6bf68b4c9d9671340c5
HKStationReport <53723569+HKStationReport@users.noreply.github.com>
2019-08-07 15:44:59 +0800

    Update data.js

--- data.js
+++ data.js
@@ -17,6 +17,34 @@ var locationGPSBotList =
 
 };
 
+/*
+"Tin Shui Wai Police Station":{"GPS":{"Lat":22.449879563238113,"Long":114.00237398494641},"BotToken":"aaaaaa"},
+"Tin Shui Wai, Tin Shing Court":{"GPS":{"Lat":22.447929025913765,"Long":114.00175981558277},"BotToken":"aaaaaaaa"},
+"Tin Shui Wai, Tin Yiu Estate":{"GPS":{"Lat":22.451380539608827,"Long":114.00476938283111},"BotToken":"aaaaaaaaaaa"},
+"Tin Shui Wai, Tin Tsz Estate":{"GPS":{"Lat":22.45295383939372,"Long":114.00767349068417},"BotToken":"aaaaaaaa"},
+"Tin Shui Wai, Locwood Court":{"GPS":{"Lat":22.45179224216722,"Long":114.00026507077882},"BotToken":"aaaaaaaa"},
+"Tin Shui Wai, Tin Shui Estate":{"GPS":{"Lat":22.455878906749575,"Long":113.9980166207149},"BotToken":"aaaaaaaaaaaaa"},
+"Tin Shui Wai, Tin Wah Estate":{"GPS":{"Lat":22.460511668621663,"Long":113.99577484038116},"BotToken":"aaaaa"},
+"Tin Shui Wai, Tin Yan Estate":{"GPS":{"Lat":22.463560338153833,"Long":113.99627598239567},"BotToken":"aaaaaaaaaaaa"},
+"Tin Shui Wai, Tin Chak Estate":{"GPS":{"Lat":22.467856900632487,"Long":113.99797158576439},"BotToken":"aaaaaaaaaaa"},
+"Tin Shui Wai, Tin Heng Estate":{"GPS":{"Lat":22.470499094055736,"Long":114.00000561692627},"BotToken":"aaaaaaaaaaa"},
+"Tin Shui Wai, Grandeur Terrace":{"GPS":{"Lat":22.46919990238574,"Long":114.00151345822087},"BotToken":"aaaaaaaaaaa"},
+"Tin Shui Wai, Tin Yat Estate":{"GPS":{"Lat":22.46666864318357,"Long":114.00003285215712},"BotToken":"aaaaaaaaaaa"},
+"Tin Shui Wai, One Sky Mall":{"GPS":{"Lat":22.46474622630481,"Long":114.00327018619293},"BotToken":"aaaaaaaaaaa"},
+"Tin Shui Wai, Tin Fu Estate":{"GPS":{"Lat":22.464758664055772,"Long":113.9997358583081},"BotToken":"aaaaaaaaaaa"},
+"Tin Shui Wai, Tin Yuet Estate":{"GPS":{"Lat":22.463212488234362,"Long":114.00028278550655},"BotToken":"aaaaaaaaaaa"},
+"Tin Shui Wai, Tin Chung Estate":{"GPS":{"Lat":22.460796119975544,"Long":113.99933047494278},"BotToken":"aaaaaaaaaaa"},
+"Tin Shui Wai, Chestwood Court":{"GPS":{"Lat":22.458105022097363,"Long":113.99943509113655},"BotToken":"aaaaaaaaaaa"},
+"Tin Shui Wai, Fortune Malls":{"GPS":{"Lat":22.457796480958073,"Long":114.00360880749576},"BotToken":"aaaaaaaaaaa"},
+"Tin Shui Wai, Sherwood Court":{"GPS":{"Lat":22.453991624828298,"Long":114.00210626420585},"BotToken":"aaaaaaaaaaa"},
+"Tin Shui Wai, Tin Wu Station":{"GPS":{"Lat":22.454970509791732,"Long":114.00574922561645},"BotToken":"aaaaaaaaaaa"},
+"Tin Shui Wai, Tin Pak Road Park":{"GPS":{"Lat":22.457022977376155,"Long":114.0068382024765},"BotToken":"aaaaaaaaaaa"},
+"Tin Shui Wai, Kenswood Court":{"GPS":{"Lat":22.458331781416962,"Long":114.00593161582947},"BotToken":"aaaaaaaaaaa"},
+"Tin Shui Wai, Maywood Court":{"GPS":{"Lat":22.461266630398462,"Long":114.0061891078949},"BotToken":"aaaaaaaaaaa"},
+"Tin Shui Wai, Lynwood Court":{"GPS":{"Lat":22.460433774165277,"Long":114.00403261184692},"BotToken":"aaaaaaaaaaa"},
+"Tin Shui Wai, Tin Yip Road Park":{"GPS":{"Lat":4477,"Long":00400000000002},"BotToken":"aaaaaaaaaaa"},
+*/
+
 var keys = Object.keys(locationGPSBotList); //not work
 console.log('keys contains ' + keys.length + ' keys: '+  keys);
 console.log(keys[0]);
