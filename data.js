23fcb16ffd764a978559180cb293f439c7440bf6
HKStationReport <53723569+HKStationReport@users.noreply.github.com>
2019-08-07 14:31:37 +0800

    Update data.js

--- data.js
+++ data.js
@@ -7,13 +7,13 @@ var locationGPSBotList =
   "Long Ping":{"GPS":{"Lat":22.447682517080697,"Long":114.02546882629394},"BotToken":"F4rK7vwoJELnIizSJvFp"},
   "Siu Hong":{"GPS":{"Lat":22.411782326705474,"Long":113.97899150848388},"BotToken":"6UbGwV5uylwARxkFoYoY"},
   "Tuen Mun":{"GPS":{"Lat":22.394880239860974,"Long":113.97311210632324},"BotToken":"BO1RJMHSqifoav9eySwJ"},
-  "Kam Sheung Road":{"GPS":{"Lat":22.434791252075236,"Long":114.06349182128906},"BotToken":""},
-  "Tsuen Wan West":{"GPS":{"Lat":22.36845173361036,"Long":114.10962581634521},"BotToken":""},
-  "Mei Foo":{"GPS":{"Lat":22.33759228736289,"Long":114.1379714012146},"BotToken":""},
-  "Nam Cheong":{"GPS":{"Lat":22.32679482923836,"Long":114.15367841720581},"BotToken":""},
-  "Austin":{"GPS":{"Lat":22.3052966666944,"Long":114.1661024093628},"BotToken":""},
-  "East Tsim Sha Tsui":{"GPS":{"Lat":22.29483422136273,"Long":114.17341947555542},"BotToken":""},
-  "Hung Hom":{"GPS":{"Lat":22.30335114793344,"Long":114.18144464492798},"BotToken":""}
+  "Kam Sheung Road":{"GPS":{"Lat":22.434791252075236,"Long":114.06349182128906},"BotToken":"OHVS1duq3DrVbRA2tNGJ"},
+  "Tsuen Wan West":{"GPS":{"Lat":22.36845173361036,"Long":114.10962581634521},"BotToken":"7eE3jmOHvAZs8hEMBZRi"},
+  "Mei Foo":{"GPS":{"Lat":22.33759228736289,"Long":114.1379714012146},"BotToken":"UZ7OMofY8Zv8a46xGpHk"},
+  "Nam Cheong":{"GPS":{"Lat":22.32679482923836,"Long":114.15367841720581},"BotToken":"aXOQrbNT3Err7f2zl3qB"},
+  "Austin":{"GPS":{"Lat":22.3052966666944,"Long":114.1661024093628},"BotToken":"yXl6LBiYuKfWsUCj8ucH"},
+  "East Tsim Sha Tsui":{"GPS":{"Lat":22.29483422136273,"Long":114.17341947555542},"BotToken":"jQKC6LBYVskHUiJKySjy"},
+  "Hung Hom":{"GPS":{"Lat":22.30335114793344,"Long":114.18144464492798},"BotToken":"1N7z5PS17WQ6OTMFzrY4"}
 
 };
 
